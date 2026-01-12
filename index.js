985ba2f6ff431512f09578d85957501dea565cb5
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-13 00:55:16 +0530

    Add cors settings and accept urlencoding and json in backend

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config({
+  path: "./.env",
+});
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "Options"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PROT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.get("/about-us", (req, res) => {
+  res.send("This is about us page");
+});
+
+app.get("/contact-us", (req, res) => {
+  res.send("This is contact us page");
 });
 
 app.listen(port, () => {
