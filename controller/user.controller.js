70b6a02d3493a9e7076f4808b29d808961fd2fea
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-15 23:58:55 +0530

    Fix registerUser method

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,19 +1,17 @@
 import User from "../model/User.model.js";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
-
 const registerUser = async (req, res) => {
   // get data
-  // validate data
+  //validate
   // check if user already exists
-  // if not, create a new user in database
-  // create a verification token
+  // create a user in database
+  //create a verification token
   // save token in database
   // send token as email to user
   // send success status to user
 
   const { name, email, password } = req.body;
-
   if (!name || !email || !password) {
     return res.status(400).json({
       message: "All fields are required",
@@ -21,10 +19,10 @@ const registerUser = async (req, res) => {
   }
 
   try {
-    const existigUser = User.findOne({ email });
-    if (existigUser) {
-      res.send(400).json({
-        message: "User already exist",
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
       });
     }
 
@@ -33,52 +31,51 @@ const registerUser = async (req, res) => {
       email,
       password,
     });
-
-    console.log("user is", user);
+    console.log(user);
 
     if (!user) {
-      res.status(400).json({
+      return res.status(400).json({
         message: "User not registered",
       });
     }
-    const token = crypto.randomBytes().toString("hex");
-    console.log("token is ", token);
 
+    const token = crypto.randomBytes(32).toString("hex");
+    console.log(token);
     user.verificationToken = token;
 
     await user.save();
 
-    // send email
-
+    //send email
     const transporter = nodemailer.createTransport({
       host: process.env.MAILTRAP_HOST,
       port: process.env.MAILTRAP_PORT,
-      secure: false, // Use true for port 465, false for port 587
+      secure: false, // true for port 465, false for other ports
       auth: {
         user: process.env.MAILTRAP_USERNAME,
         pass: process.env.MAILTRAP_PASSWORD,
       },
     });
 
-    const mailOptions = {
-      from: process.env.MAILTRAP_PASSWORD,
+    const mailOption = {
+      from: process.env.MAILTRAP_SENDEREMAIL,
       to: user.email,
-      subject: "Verify your email",
-      text: `Please click on the following link: ${process.env.BASE_URL}/api/v1/users/verify${token}`, // Plain-text version of the message
-      // html: "<b>Hello world?</b>", // HTML version of the message
+      subject: "Verify your email", // Subject line
+      text: `Please click on the following link:
+      ${process.env.BASE_URL}/api/v1/users/verify/${token}
+      `,
     };
 
-    await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOption);
 
-    res.status(200).json({
-      message:
-        "User registered successfully. Please check your email to verify your account.",
+    res.status(201).json({
+      message: "User registered successfully",
+      success: true,
     });
   } catch (error) {
     res.status(400).json({
-      message: "Error in registering user",
-      success: false,
+      message: "User not registered ",
       error,
+      success: false,
     });
   }
 };
