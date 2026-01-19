50900d0b3e2656ee31da376cc82e1976c33e9964
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-20 01:00:15 +0530

    Create Auth Middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+
+export const isLoggedIn = async (req, resizeBy, next) => {
+  // get token from cookie
+  // check token
+  // get data from token
+
+  try {
+    console.log(req.cookies);
+    let token = req.cookies?.token;
+    console.log("Token found", token ? "YES" : "NO");
+    if (!token) {
+      console.log("NO token");
+      return res.status(401).json({
+        success: false,
+        message: "Authentication failed",
+      });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(`decoded data ${decoded}`);
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    console.log(`Auth middleware failure`);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+  next();
+};
